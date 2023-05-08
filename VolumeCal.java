ec270593a6bfd60abc2c21e40092984893630b19
Gerald Polk <89165456+GeraldjShine@users.noreply.github.com>
2023-05-08 14:57:14 -0400

    VolumeCal.java

--- VolumeCal.java
+++ VolumeCal.java
@@ -3,8 +3,8 @@ import java.util.*;
 /* 
  * Author:        Gerald J Polk 
  * Date:          01/30/2023
- * Description:   A program to calculate volume in cubic ft & gallons per cubic ft.
- * C-Lang:        Java
+ * Description:   A program to calculate the volume in cubic ft & gallons per cubic ft.
+ * Lang:          Java
  *
  */
 public class VolumeCal {
@@ -15,36 +15,36 @@ public class VolumeCal {
 	public static final double GAL_PERcuFT = 7.5;
 	public static void main(String[] args) {
    
-		// Declare variables
-      int wIN, lIN, dIN;
-		double wFT, lFT, dFT, cuFT, gal;
+	// Declare variables
+      	int wIN, lIN, dIN;
+	double wFT, lFT, dFT, cuFT, gal;
       
-		// Prompt for and input Width in inches
-		System.out.print("Enter width in inches: ");
-		   wIN = console.nextInt();
+	// Prompt for and input Width in inches
+	System.out.print("Enter width in inches: ");
+		wIN = console.nextInt();
       
-		// Prompt for and input the Length in inches
-      System.out.print("Enter length in inches: ");
-		   lIN = console.nextInt();
+	// Prompt for and input the Length in inches
+      	System.out.print("Enter length in inches: ");
+		lIN = console.nextInt();
       
-		// Prompt for and input the Depth in inches
-      System.out.print("Enter depth in inches: ");
-		   dIN = console.nextInt();
+	// Prompt for and input the Depth in inches
+      	System.out.print("Enter depth in inches: ");
+		dIN = console.nextInt();
       
-		// Convert Length, Width & Depth to ft
-		wFT = wIN / 12.0;
-		lFT = lIN / 12.0;
-		dFT = dIN / 12.0;
+	// Convert Length, Width & Depth to ft
+	wFT = wIN / 12.0;
+	lFT = lIN / 12.0;
+	dFT = dIN / 12.0;
 
-		// Calculate # of cuft & gallons per cuft
-      cuFT = wFT * lFT * dFT;
-		gal = cuFT * GAL_PERcuFT;
+	// Calculate # of cuft & gallons per cuft
+      	cuFT = wFT * lFT * dFT;
+	gal = cuFT * GAL_PERcuFT;
       
-		// Display volume in cuft
-      System.out.printf("The objects volume is %.2f cubic feet.%n", cuFT);
+	// Display volume in cuft
+      	System.out.printf("The objects volume is %.2f cubic feet.%n", cuFT);
 
-		// Display # of gallons
-      System.out.printf("That makes %.0f gallons.", gal);
+	// Display # of gallons
+      	System.out.printf("That makes %.0f gallons.", gal);
 
 	}
 }
