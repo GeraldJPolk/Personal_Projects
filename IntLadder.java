45855e95c8cf483dd686f644fec39a8379d84677
Gerald Polk <89165456+GeraldjShine@users.noreply.github.com>
2023-05-10 08:58:27 -0400

    IntLadder.java

--- IntLadder.java
+++ IntLadder.java
@@ -10,41 +10,41 @@ import java.util.*;
 public class IntLadder {
   
 	public static final Scanner console = new Scanner(System.in);
-  	public static void main(String[] args) {
+  public static void main(String[] args) {
       
 		// Declare Variables
-      	int num1, num2, num3;
+    int num1, num2, num3;
       
-      	// Prompt & input for ints
-      	System.out.printf("Enter three integers, pressing enter/space after each.%n");
-        	num1 = console.nextInt();
-        	num2 = console.nextInt();
-         	num3 = console.nextInt();
+    // Prompt & input for ints
+    System.out.printf("Enter three integers, pressing enter/space after each.%n");
+    	num1 = console.nextInt();
+      num2 = console.nextInt();
+      num3 = console.nextInt();
          
-      	// Order ints based on Lowest to Highest
-      	System.out.printf("%nThe listed integers from lowest to highest are: ");
+    // Order ints based on Lowest to Highest
+    System.out.printf("%nThe listed integers from lowest to highest are: ");
       
-      	if (num1 < num2) {
-         	if (num1 < num3) {
-            	if (num2 < num3) {
-               		System.out.printf("%d %d %d", num1, num2, num3);
-            	} else {
-               		System.out.printf("%d %d %d", num1, num3, num2);
-            	}   
-         	} else {
-            	System.out.printf("%d %d %d", num3, num1, num2);
-         	}
+    if (num1 < num2) {
+    	if (num1 < num3) {
+      	if (num2 < num3) {
+        	System.out.printf("%d %d %d", num1, num2, num3);
+        } else {
+          System.out.printf("%d %d %d", num1, num3, num2);
+        }   
+      } else {
+      	System.out.printf("%d %d %d", num3, num1, num2);
+      }
          
-      	} else if (num2 < num3) {
-         	if (num1 < num3) {
-            	System.out.printf("%d %d %d", num2, num1, num3);      
-         	} else {
-            	System.out.printf("%d %d %d", num2, num3, num1);
-         	}         
-      	} else {
-         	System.out.printf("%d %d %d", num3, num2, num1);
-      	}
+    } else if (num2 < num3) {
+    	if (num1 < num3) {
+      	System.out.printf("%d %d %d", num2, num1, num3);      
+      } else {
+      	System.out.printf("%d %d %d", num2, num3, num1);
+      }         
+    } else {
+    	System.out.printf("%d %d %d", num3, num2, num1);
+    }
       
-      	System.out.println();
+    System.out.println();
 	}
 }
